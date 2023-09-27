c798d91c0f09e8c4b0657370cfd2ab9f847c9964
baiyi <421586319@qq.com>
2023-09-27 22:25:24 +0800

    make sure use max_ndet

--- src/char-coeff-new-indexing.jl
+++ src/char-coeff-new-indexing.jl
@@ -211,7 +211,7 @@ function MathieuExponent(a,q;ndet::Int=20,has_img::Bool=true,max_ndet::Int=1000)
             _,idx=findmin(abs,vals)
             return ν,vecs[:,idx]
         elseif ndet<max_ndet
-            MathieuExponent(a,q;ndet=2*ndet,has_img=false,max_ndet=max_ndet)
+            MathieuExponent(a,q;ndet=min(2*ndet,max_ndet),has_img=false,max_ndet=max_ndet)
         else
             @warn "Expect real output for a=$a and q=$q, but the result is complex even for ndet=$ndet."
             alpha=acos(2*Complex(delta)-1)/pi
